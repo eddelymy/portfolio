3301660df804285ac08045fd4db83b2f759091e2
Amina EDDELYMY <a.eddelymy@azura-group.com>
2024-08-05 15:28:28 +0100

    update design of skillsView

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,7 +1,32 @@
 <template>
-
+  <div class="w-full h-full py-36">
+    <h1 class="pb-2 text-3xl border-b"
+      :class="darkMode ? ' border-white' : 'border-black' ">
+      Projets
+    </h1>
+    <div class="h-full w-full flex items-center">
+      <div 
+        class="rounded-md text-center justify-items-center p-7 bg-opacity-50 w-[390px] flex flex-col"
+        :class="!darkMode ? 'bg-white' :'bg-black'">
+        My first projet
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import {storeToRefs} from "pinia"
+import {useDarkModeStore} from '../stores/darkMode'
+import {ref,onMounted} from 'vue'
+
+const darkModeStore = useDarkModeStore()
+const {darkMode} = storeToRefs(darkModeStore)
+
+const projects = ref([])
+
+
+
+
+</script>
 
 <style scoped></style>
--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -4,24 +4,16 @@
       :class="darkMode ? ' border-white' : 'border-black' ">
       Compétence
     </h1>
-    <div class="mt-16 flex flex-row p-4 min-h-52">
+    <div class="scrollbar-custom mx-auto max-w-[700px] h-[400px] overflow-y-scroll grid grid-cols-4 gap-5 mt-16 px-2 ">
       <div 
-        class="rounded-md h-full bg-opacity-50 w-full p-6 flex flex-wrap justify-between "
-        :class="!darkMode ? ' bg-white' : 'bg-black' ">
-        <div class="" v-for="(skill,index) in skills" :key="index">
-          <h1 class="text-2xl">
-            {{ skill.title }}
-          </h1>
-          <div class="mt-4 flex flex-wrap justify-between"
-            v-for="techno in skill.children">
-            <span class=" text-white rounded-md px-3"
-              :class="darkMode ? 'bg-[#3b82f6]' : 'bg-[#a855f7]'">
-                {{ techno.title }}
-                <!-- #3b82f6 -->
-            </span>
-          </div>
+        class=" text-wrap cursor-pointer hover:-translate-y-1 hover:scale-10 duration-300 rounded-md bg-opacity-50 h-[125px] grid grid-rows-2 place-items-center py-3"
+        v-for="(skill,index) in skills" :key="index"
+          :class="!darkMode ? ' bg-white' : 'bg-black' ">
+          <img :src="!darkMode ? `/` + skill.light_logo : `/` + skill.dark_logo "  
+            class="mx-auto"
+            :width="skill.width"/>
+          <span>{{ skill.title }}</span>
         </div>
-      </div>
     </div>
   </div>
 </template>
@@ -32,97 +24,121 @@ import {useDarkModeStore} from '../stores/darkMode'
 import {ref} from 'vue'
 
 const skills = ref([
-  {
-    title:'Langages de programmation',
-    children:[{
-      title:'Python'
+    {
+      title:'Python',
+      light_logo:'python-light.svg',
+      dark_logo:'python-dark.svg',
+      width:"50"
     },
     {
-      title:'Java'
+      title:'Java',
+      light_logo:'Java_Logo-light.svg',
+      dark_logo:'Java_Logo-dark.svg',
+      width:"50"
     },
     {
-      title:'Php'
-    }]
-  },
-  {
-    title:'Développement web/mobile',
-    children:[{
-      title:'HTML'
+      title:'Php',
+      light_logo:'PHP-logo-light.svg',
+      dark_logo:'PHP-logo-dark.svg',
+      width:"50"
     },
     {
-      title:'JavaScript'
+      title:'HTML',
+      light_logo:'html-logo-light.svg',
+      dark_logo:'html-logo-dark.svg',
+      width:"50"
     },
     {
-      title:'VueJS'
+      title:'JavaScript',
+      light_logo:'javascript-light.svg',
+      dark_logo:'javascript-dark.svg',
+      width:"50"
     },
     {
-      title:'ReactJS'
+      title:'VueJS',
+      light_logo:'vuejs-light.svg',
+      dark_logo:'vuejs-dark.svg',
+      width:"50"
     },
     {
-      title:'Android'
+      title:'ReactJS',
+      light_logo:'react-light.svg',
+      dark_logo:'react-dark.svg',
+      width:"50"
     },
     {
-      title:'Bootstrap'
+      title:'Android',
+      light_logo:'android-light.svg',
+      dark_logo:'android-dark.svg',
     },
     {
-      title:'Tailwind'
+      title:'Bootstrap',
+      light_logo:'bootstrap-light.svg',
+      dark_logo:'bootstrap-dark.svg',
     },
     {
-      title:'Bulma'
-    }]
-  },
-  {
-    title:'Bases de données',
-    children:[{
-      title:'Mongodb'
+      title:'Tailwind',
+      light_logo:'tailwind-light.svg',
+      dark_logo:'tailwind-dark.svg',
     },
     {
-      title:'Hbase'
+      title:'Mongodb',
+      light_logo:'mongodb-light.svg',
+      dark_logo:'mongodb-dark.svg',
     },
     {
-      title:'BDR'
-    }]
-  },
-  {
-    title:'Langages de requête ',
-    children:[{
-      title:'PL/SQL'
+      title:'MySQL',
+      light_logo:'mysql-light.svg',
+      dark_logo:'mysql-dark.svg',
+      width:'50'
     },
     {
-      title:'SQL'
-    }]
-  },
-  {
-    title:'Science de données',
-    children:[{
-      title:'Algorithmes Machine Learning'
+      title:'SQL',
+      light_logo:'sql-light.svg',
+      dark_logo:'sql-dark.svg',
+      width:"50"
     },
     {
-      title:'Algorithmes Deep Learning'
+      title:'Machine learning',
+      light_logo:'machine-learning-light.svg',
+      dark_logo:'machine-learning-dark.svg',
+      width:'50'
     },
     {
-      title:'Statistique et probabilité'
-    }]
-  },
-  {
-    title:'BigData/Business Intelligence',
-    children:[{
-      title:'Hadoop'
+      title:'Deep learning',
+      light_logo:'deep-learning-light.svg',
+      dark_logo:'deep-learning-dark.svg',
+      width:"50"
     },
     {
-      title:'Appache Hive'
+      title:'Hadoop',
+      light_logo:'hadoop-light.svg',
+      dark_logo:'hadoop-dark.svg',
+      width:'50'
     },
     {
-      title:'Pentaho'
+      title:'Apache Hive',
+      light_logo:'hive-light.svg',
+      dark_logo:'hive-dark.svg',
+      width:'50'
     },
     {
-      title:'Apache Spark'
-    }]
-  },
+      title:'Apache Spark',
+      light_logo:'spark-light.svg',
+      dark_logo:'spark-dark.svg',
+      width:'50'
+    }
+    // ,
+    // {
+    //   title:'Pentaho',
+    //   light_logo:'python-light.svg',
+    //   dark_logo:'Java_Logo-dark.svg',
+    // }
 ])
 
 const darkModeStore = useDarkModeStore()
 const {darkMode} = storeToRefs(darkModeStore)
+
 </script>
 
 <style scoped></style>
